54066c9e67e9638cdc90d1f377f506bd8960190e
wangyihui <yihui.wang@pku.edu.cn>
2020-11-03 12:02:33 +0800

    Ini：0.2.1

--- src/main/java/cc/mbin/ListNode.java
+++ src/main/java/cc/mbin/ListNode.java
@@ -26,12 +26,14 @@ public class ListNode {
         this.next = next;
     }
 
-    ListNode(){}
-    ListNode(int val){
+    public ListNode(){}
+
+    public ListNode(int val){
         this.val = val;
         this.next = null;
     }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
     @Override
     public String toString() {
         return "{ val:"+ val + "  next:"+ next + "}";
