3f0b0c9f1999d2b0e382bc93317ca09ee32f7829
wangyihui <yihui.wang@pku.edu.cn>
2020-11-03 11:58:29 +0800

    Ini：0.2.1

--- src/main/java/cc/mbin/ListNode.java
+++ src/main/java/cc/mbin/ListNode.java
@@ -7,8 +7,25 @@ package cc.mbin;
  * @version:1.0.0
  */
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
     ListNode(){}
     ListNode(int val){
         this.val = val;
